56a0a607a7598f099f99abd0a7afdb9758de82ab
JiaxiangYi96 <yagafighting@gmail.com>
2025-11-04 14:17:19 +0100

    load dataset

---
+++ src/MFVeBRNN/__init__.py
@@ -0,0 +1,27 @@
+"""
+bessa-pypi-template - Template repository for Python packages following
+the Bessa Group's conventions
+
+This package contains a template for Python packages following
+the Bessa Group's conventions.
+
+Usage:
+  import bessa-pypi-template
+
+Author: Your Name (youremailadress@email.com)
+"""
+
+#                                                                       Modules
+# =============================================================================
+
+
+#                                                        Authorship and Credits
+# =============================================================================
+__author__ = 'Your Name (youremailadress@email.com)'
+__credits__ = ['Your Name']
+__status__ = 'Stable'
+# =============================================================================
+#
+# =============================================================================
+
+from .dataset.load_dataset import SingleFidelityDataset, MultiFidelityDataset
